926c55609ed423a1ee23ef116319f62cb9f03796
Nezz <bloodsource@vipmail.hu>
2015-02-04 02:44:23 +0100

    Log failed and passed tests Ability to skip specified tests (like passed ones) Improved runner looks

--- NUnit.SocketRunner/Program.cs
+++ NUnit.SocketRunner/Program.cs
@@ -24,7 +24,9 @@ namespace NUnit.SocketRunner
             {
                 client.Connect(ip, 4711);
 
-                Console.WriteLine("Connected. Specify tests to run? [y] [n] [f]");
+                Console.WriteLine("Connected");
+
+                Console.WriteLine("Specify tests to run? [y] [n] [f]");
                 var line = Console.ReadLine();
 
                 if (line.StartsWith("y")) // Yes
@@ -42,13 +44,58 @@ namespace NUnit.SocketRunner
                 else // No
                     WriteLine(client, line);
 
-                line = "Running tests...";
-                while (!line.StartsWith("Run finished"))
+                Console.WriteLine("Specify tests to exclude? [y] [n] [f]");
+                line = Console.ReadLine();
+
+                bool passFile = false;
+
+                if (line.StartsWith("y")) // Yes
                 {
-                    Console.WriteLine(line);
-                    line = ReadLine(client);
+                    WriteLine(client, line);
+                    Console.WriteLine("Enter tests to exclude, comma separated with full name in a single line:");
+                    var tests = Console.ReadLine();
+                    WriteLine(client, tests);
+                }
+                else if (line.StartsWith("f")) // File
+                {
+                    WriteLine(client, "y");
+                    WriteLine(client, File.ReadAllText("passed.txt"));
+                    passFile = true;
+                }
+                else // No
+                    WriteLine(client, line);
+
+                line = "Running tests...";
+
+                using (var fs = new FileStream("failed.txt", FileMode.Create))
+                using (var ps = new FileStream("passed.txt", passFile ? FileMode.Append : FileMode.Create))
+                using (var fw = new StreamWriter(fs))
+                using (var pw = new StreamWriter(ps))
+                {
+                    while (!line.StartsWith("Run finished"))
+                    {
+                        //Console.WriteLine(line);
+
+                        if (line.StartsWith("Passed "))
+                        {
+                            Console.Write(".");
+                            pw.Write(line.Substring("Passed ".Length) + ",");
+                            pw.Flush();
+                        }
+                        else if (line.StartsWith("Failed "))
+                        {
+                            Console.Write("X");
+                            fw.Write(line.Substring("Failed ".Length) + ",");
+                            fw.Flush();
+                        }
+                        else if (line.StartsWith("Skipped "))
+                            Console.Write(">");
+
+                        line = ReadLine(client);
+                    }
                 }
 
+                Console.WriteLine();
                 Console.WriteLine(line);
 
                 var xml = ReadLine(client).Replace("**RETURN**", "\r\n");
