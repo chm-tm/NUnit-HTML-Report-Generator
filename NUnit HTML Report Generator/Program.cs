25cb8aee3bd82f651bf0456757366ce49cef98e5
Nezz <bloodsource@vipmail.hu>
2015-02-07 15:54:26 +0100

    Fixed build

--- NUnit HTML Report Generator/Program.cs
+++ NUnit HTML Report Generator/Program.cs
@@ -528,7 +528,7 @@ namespace Jatech.NUnit
                 // Add failure messages if available
                 if (testCase.Elements("failure").Count() == 1)
                 {
-                    html.AppendLine(string.Format("<div><strong>Message:</strong> {0}</div>", WebUtility.HtmlEncode(testCase.Element("failure").Element("message").Value));
+                    html.AppendLine(string.Format("<div><strong>Message:</strong> {0}</div>", WebUtility.HtmlEncode(testCase.Element("failure").Element("message").Value)));
                     html.AppendLine(string.Format("<div><strong>Stack Trace:</strong> <pre>{0}</pre></div>", testCase.Element("failure").Element("stack-trace") == null ? "N/A" : WebUtility.HtmlEncode(testCase.Element("failure").Element("stack-trace").Value)));
                 }
 
